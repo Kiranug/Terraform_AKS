b3a03ad392d988215bbc102b73a228b8ba227330
Kiranug <kiran.ug123@gmail.com>
2025-01-18 20:34:18 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,34 @@
+module "resource_group" {
+  source              = "./modules/resource_group"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+}
+
+module "vnet" {
+  source              = "./modules/vnet"
+  resource_group_name = module.resource_group.resource_group_name
+  location            = module.resource_group.resource_group_location
+  vnet_name           = var.vnet_name
+  address_space       = var.address_space
+  subnets             = var.subnets
+}
+
+module "aks" {
+  source              = "./modules/aks"
+  ...
+}
+
+module "load_balancer" {
+  source              = "./modules/load_balancer"
+  ...
+}
+
+module "key_vault" {
+  source              = "./modules/key_vault"
+  ...
+}
+
+module "database" {
+  source              = "./modules/database"
+  ...
+}
